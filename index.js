bfb569402580092688536de69a6307e312d3603d
rkoko <rinda.ko@gmail.com>
2017-06-27 17:45:26 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
